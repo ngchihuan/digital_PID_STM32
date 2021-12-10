b675e6730420037ae8e235c74cf6e119ce9efa3a
ngchihuan <ngchihuan@gmail.com>
2021-12-10 16:34:10 +0800

    working on adding devices into the combobox

--- software/src/ADC_DAC_main.py
+++ software/src/ADC_DAC_main.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import QObject,pyqtSlot
 
 from pid_controller import DAC_conv_fac_ltc2785_16bit
 from pid_controller import returnSpanMode
+from pid_controller import search_ctr_boards
 
 class ADC_DAC(QtWidgets.QWidget):
     
@@ -12,8 +13,30 @@ class ADC_DAC(QtWidgets.QWidget):
         self.setWindowTitle("ADC_DAC")
         layout = QtWidgets.QGridLayout()
         self.setLayout(layout)
+    
         
-        #adc1_box = QtWidgets.QGroupBox()
+        self.device_box = QtWidgets.QGroupBox()
+        self.device_box.setTitle("Device")
+        self.device_form = QtWidgets.QFormLayout(self.device_box)
+        self.deviceComboBox = QtWidgets.QComboBox()
+        
+        
+        self.search_button = QtWidgets.QPushButton('Search')
+        self.connect_button = QtWidgets.QPushButton('Connect')
+        self.disconnect_button = QtWidgets.QPushButton('Disconnect')
+        
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addWidget(self.connect_button)
+        button_layout.addWidget(self.search_button)
+        button_layout.addWidget(self.disconnect_button)
+        
+        self.device_form.addRow(QtWidgets.QLabel("Device"),self.deviceComboBox)
+        self.device_form.addRow(button_layout)
+        
+    
+        
+        
+        
         self.adc1 = ADC("ADC 1")
         self.adc2 = ADC("ADC 2")
         self.dac1 = DAC( "DAC 1")
@@ -24,15 +47,31 @@ class ADC_DAC(QtWidgets.QWidget):
         layout.addWidget(self.adc2.adc_box, 0, 1)
         layout.addWidget(self.dac1.dac_box, 1, 0)
         layout.addWidget(self.dac2.dac_box, 1, 1)
+        layout.addWidget(self.device_box,2,0)
         
-        #self.dac1.setSpanInput.valueChanged.connect(self.updateVoltSpan)
+        #list device
+        self.update_device_list()
         
-    def updateVoltSpan(self):
+        #self.comboSerialBox.currentText()
+    def update_device_list(self):
+        ctrlist = search_ctr_boards()
+        if len(ctrlist)!=0:
+            for i in ctrlist:
+                self.deviceComboBox.addItem(i)
+                
+                
+    
+    def updateVolt(self):
         print('test')
         
+
+
+        #self.device_box= 1.0
+        #self.device_box.setTitle("Device")
+        #self.device_form = QtWidgets.QFormLayout(self.device_box)
         
         
-class ADC():
+class ADC(QObject):
     def __init__(self,name):
         self.adc_box = QtWidgets.QGroupBox()
         self.name = name
